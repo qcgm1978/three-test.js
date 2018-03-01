aff78cc5ff5b959019d015627ffafd2b0ea6add6
zhanghl-k <zhanghl-k@glodon.com>
2018-03-01 18:04:08 +0800

    debug

--- test/custom/three.spec.js
+++ test/custom/three.spec.js
@@ -4,7 +4,7 @@ import { AnimationClip } from '../../src/animation/AnimationClip';
 // import { NumberKeyframeTrack } from '../../../../src/animation/tracks/NumberKeyframeTrack';
 // import { Object3D } from '../../../../src/core/Object3D';
 // import { LoopOnce, LoopRepeat, LoopPingPong } from '../../../../src/constants';
-
+import { FileLoader} from '../../src/loaders/FileLoader'
 export default QUnit.module('Constants', () => {
 
     QUnit.test("test", (assert) => {
@@ -21,4 +21,28 @@ export default QUnit.module('Constants', () => {
         assert.ok(animationAction, "animationAction instanciated");
 
     });
+    QUnit.test('FileLoader',(assert)=>{
+        var loader=new FileLoader()
+        assert.ok(loader);
+        loader.load(
+	// resource URL
+	'example.txt',
+
+	// onLoad callback
+	function ( data ) {
+		// output the text to the console
+		console.log( data )
+	},
+
+	// onProgress callback
+	function ( xhr ) {
+		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+	},
+
+	// onError callback
+	function ( err ) {
+		console.error( 'An error happened' );
+	}
+);
+    })
 });
